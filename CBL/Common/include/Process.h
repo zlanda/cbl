bf0fe2c3cec0fb3aa5be5509c76002d92f9435b9
zlanda <zhanxc_chpu@sina.com>
2018-07-15 18:41:24 +0800

    add common test

---
+++ CBL/Common/include/Process.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+*   文  件 名：Process.h
+*   功     能：进程对外接口
+*   作     者：zhanxc
+*   E-Mail   : zhanxc_chpu@sina.com
+*   创建日期：018-7-14
+*   修改历史：无
+*******************************************************************************/
+
+#ifndef  _CBL_COMMON_PROCESS_H_
+#define  _CBL_COMMON_PROCESS_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 内部头文件 */
+#include "Types.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
---
+++ CBL/Common/include/Thread.h
@@ -0,0 +1,27 @@
+/*******************************************************************************
+*   文  件 名：Thread.h
+*   功     能：线程对外接口
+*   作     者：zhanxc
+*   E-Mail   : zhanxc_chpu@sina.com
+*   创建日期：018-7-14
+*   修改历史：无
+*******************************************************************************/
+
+#ifndef  _CBL_COMMON_THREAD_H_
+#define  _CBL_COMMON_THREAD_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* 内部头文件 */
+#include "Types.h"
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
